de7447d7759b453818f834177afaa8b747d24b5a
Harry Poulter <harry.poulter@adelaide.edu.au>
2017-08-19 18:16:37 +0930

    Made code PEP8-compliant

--- slurpy/slurm.py
+++ slurpy/slurm.py
@@ -8,66 +8,112 @@ import numpy as np
 
 DECODE_FORMAT = 'utf-8'
 
-JOB_FMT = {'ACCOUNT'          : r'%a',
-           'GRES'             : r'%b',
-           'MIN_CPUS'         : r'%c',
-           'MIN_TMP_DISK'     : r'%d',
-           'END_TIME'         : r'%e',
-           'FEATURES'         : r'%f',
-           'GROUP_NAME'       : r'%g',
-           'OVER_SUBSCRIBE'   : r'%h',
-           'JOBID'            : r'%i',
-           'NAME'             : r'%j',
-           'COMMENT'          : r'%k',
-           'TIME_LIMIT'       : r'%l',
-           'MIN_MEMORY'       : r'%m',
-           'REQ_NODES'        : r'%n',
-           'COMMAND'          : r'%o',
-           'PRIORITY_FLOAT'   : r'%p',
-           'QOS'              : r'%q',
-           'REASON'           : r'%r',
-           'ST'               : r'%t',
-           'USER'             : r'%u',
-           'RESERVATION'      : r'%v',
-           'WCKEY'            : r'%w',
-           'EXC_NODES'        : r'%x',
-           'NICE'             : r'%y',
-           'S:C:T'            : r'%z',
-           'NUM_TASKS'        : r'%A',
-           'EXEC_HOST'        : r'%B',
-           'CPUS'             : r'%C',
-           'NODES'            : r'%D',
-           'DEPENDENCY'       : r'%E',
-           'ARRAY_JOB_ID'     : r'%F',
-           'GROUP_ID'         : r'%G',
-           'SOCKETS_PER_NODE' : r'%H',
-           'CORES_PER_SOCKET' : r'%I',
-           'THREADS_PER_CORE' : r'%J',
-           'ARRAY_TASK_ID'    : r'%K',
-           'TIME_LEFT'        : r'%L',
-           'TIME'             : r'%M',
-           'NODELIST'         : r'%N',
-           'CONTIGUOUS'       : r'%O',
-           'PARTITION'        : r'%P',
-           'PRIORITY_INT'     : r'%Q',
-           'NODELIST(REASON)' : r'%R',
-           'START_TIME'       : r'%S',
-           'STATE'            : r'%T',
-           'USER'             : r'%U',
-           'SUBMIT_TIME'      : r'%V',
-           'LICENSES'         : r'%W',
-           'CORE_SPEC'        : r'%X',
-           'SCHEDNODES'       : r'%Y',
-           'WORK_DIR'         : r'%Z'
-          }
+JOB_FMT = {'ACCOUNT':          r'%a',
+           'GRES':             r'%b',
+           'MIN_CPUS':         r'%c',
+           'MIN_TMP_DISK':     r'%d',
+           'END_TIME':         r'%e',
+           'FEATURES':         r'%f',
+           'GROUP_NAME':       r'%g',
+           'OVER_SUBSCRIBE':   r'%h',
+           'JOBID':            r'%i',
+           'NAME':             r'%j',
+           'COMMENT':          r'%k',
+           'TIME_LIMIT':       r'%l',
+           'MIN_MEMORY':       r'%m',
+           'REQ_NODES':        r'%n',
+           'COMMAND':          r'%o',
+           'PRIORITY_FLOAT':   r'%p',
+           'QOS':              r'%q',
+           'REASON':           r'%r',
+           'ST':               r'%t',
+           'USER':             r'%u',
+           'RESERVATION':      r'%v',
+           'WCKEY':            r'%w',
+           'EXC_NODES':        r'%x',
+           'NICE':             r'%y',
+           'S:C:T':            r'%z',
+           'NUM_TASKS':        r'%A',
+           'EXEC_HOST':        r'%B',
+           'CPUS':             r'%C',
+           'NODES':            r'%D',
+           'DEPENDENCY':       r'%E',
+           'ARRAY_JOB_ID':     r'%F',
+           'GROUP_ID':         r'%G',
+           'SOCKETS_PER_NODE': r'%H',
+           'CORES_PER_SOCKET': r'%I',
+           'THREADS_PER_CORE': r'%J',
+           'ARRAY_TASK_ID':    r'%K',
+           'TIME_LEFT':        r'%L',
+           'TIME':             r'%M',
+           'NODELIST':         r'%N',
+           'CONTIGUOUS':       r'%O',
+           'PARTITION':        r'%P',
+           'PRIORITY_INT':     r'%Q',
+           'NODELIST(REASON)': r'%R',
+           'START_TIME':       r'%S',
+           'STATE':            r'%T',
+           'USER':             r'%U',
+           'SUBMIT_TIME':      r'%V',
+           'LICENSES':         r'%W',
+           'CORE_SPEC':        r'%X',
+           'SCHEDNODES':       r'%Y',
+           'WORK_DIR':         r'%Z'
+           }
 
 JOB_DELIM = '|'
 
-JOB_STATES = {'PENDING', 'RUNNING', 'SUSPENDED',  'CANCELLED',  'COMPLETING',  'COMPLETED', 'CONFIGURING', 'FAILED', 'TIMEOUT', 'PREEMPTED', 'NODE_FAIL', 'REVOKED', 'SPECIAL_EXIT', 'BOOT_FAIL', 'STOPPED'}
+JOB_STATES = {'PENDING',
+              'RUNNING',
+              'SUSPENDED',
+              'CANCELLED',
+              'COMPLETING',
+              'COMPLETED',
+              'CONFIGURING',
+              'FAILED',
+              'TIMEOUT',
+              'PREEMPTED',
+              'NODE_FAIL',
+              'REVOKED',
+              'SPECIAL_EXIT',
+              'BOOT_FAIL',
+              'STOPPED'}
 
-JOB_STS = {'PD', 'R', 'S', 'CA', 'CF', 'CG', 'CD', 'F', 'TO', 'PR', 'NF', 'RV', 'SE', 'BF', 'ST'} # no COMPLETING?
+JOB_STS = {'PD',
+           'R',
+           'S',
+           'CA',
+           'CF',
+           'CG',
+           'CD',
+           'F',
+           'TO',
+           'PR',
+           'NF',
+           'RV',
+           'SE',
+           'BF',
+           'ST'}
 
-NODE_STATES = {'UNKNOWN', 'DOWN', 'IDLE', 'ALLOCATED', 'ERROR', 'MIXED', 'FUTURE', 'DRAIN', 'DRAINED', 'DRAINING', 'NO_RESPOND', 'RESERVED', 'PERFCTRS', 'COMPLETING', 'POWER_DOWN', 'POWER_UP', 'FAIL', 'MAINT', 'REBOOT'}
+NODE_STATES = {'UNKNOWN',
+               'DOWN',
+               'IDLE',
+               'ALLOCATED',
+               'ERROR',
+               'MIXED',
+               'FUTURE',
+               'DRAIN',
+               'DRAINED',
+               'DRAINING',
+               'NO_RESPOND',
+               'RESERVED',
+               'PERFCTRS',
+               'COMPLETING',
+               'POWER_DOWN',
+               'POWER_UP',
+               'FAIL',
+               'MAINT',
+               'REBOOT'}
 
 
 # 211ms ± 13.3ms per loop
@@ -84,7 +130,7 @@ def query_jobs(job_properties, partition=None):
 
     Returns DataFrame of job properties."""
     job_format = JOB_DELIM.join([JOB_FMT[prop.upper()]
-                           for prop in job_properties])
+                                 for prop in job_properties])
     raw_squeue = _query_squeue(job_format, partition)
 
     return _extract_job_data(raw_squeue)
@@ -105,11 +151,10 @@ def _extract_node_feature(raw_scontrol, feature):
 # 136ms ± 2.47ms per loop
 def _extract_node_features(raw_scontrol, features):
     regex = ''.join([r'.*?{}=([^\s]+)'.format(feature)
-                     for feature in features]
-                    + ['.*?'])
+                     for feature in features] + ['.*?'])
 
     matches = re.findall(regex, raw_scontrol)
-    
+
     return pd.DataFrame(data=np.array(matches),
                         columns=features)
 
