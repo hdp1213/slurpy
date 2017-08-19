711b2b86877f411e11f4a4bac3fbfca346a0e5f1
Harry Poulter <harry.poulter@adelaide.edu.au>
2017-08-19 14:20:24 +0930

    Fixed relative module inclusion

--- slurpy/__init__.py
+++ slurpy/__init__.py
@@ -1,1 +1,2 @@
-import slurpy
+from .slurpy import filter_states
+from .slurm import query_nodes, query_jobs
--- slurpy/slurpy.py
+++ slurpy/slurpy.py
@@ -1,4 +1,4 @@
-import slurm
+from .slurm import query_nodes, query_jobs, NODE_STATES
 
 NODE_FEATURES = ['NodeName',
                  'CPUAlloc',
@@ -19,14 +19,14 @@ JOB_FEATURES = ['JOBID',
                 'SUBMIT_TIME']
 
 def main():
-    node_df = slurm.query_nodes(NODE_FEATURES)
+    node_df = query_nodes(NODE_FEATURES)
 
     print(filter_states(node_df, ['DOWN'], exclude=True))
 
 
 def filter_states(node_df, states, exclude=False):
     for state in states:
-        if state.upper() not in slurm.NODE_STATES:
+        if state.upper() not in NODE_STATES:
             raise ValueError('{} not valid state'.format(state.upper()))
 
     cond_states = '|'.join(['{}'.format(state) for state in states])
